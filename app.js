e0b8a167bad93ae02c752469c40f0d78d2353e26
Jose Santamaria <santamaria0323@gmail.com>
2021-02-17 17:06:25 -0300

    Handlebars

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'hbs');
+
 // Servir contenido estático
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.send('Home Page');
-// });
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html');
 });
+
 app.get('/elements', (req, res) => {
     res.sendFile(__dirname + '/public/elements.html');
 });
-// app.get('*', (req, res) => {
-//     res.send('Page not found');
-// });
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
 });
